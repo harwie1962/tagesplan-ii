290de3b11bd732ec65a26aa3316f19c1bfd92da0
harwie1962 <hspace@gmx.de>
2025-12-23 09:58:20 +0100

    add saturday plan

---
+++ plans/sat.js
@@ -0,0 +1,27 @@
+// plans/sat.js
+window.schedule = [
+  { time: "06:15", title: "Prednisolon", description: "", calories: 0 },
+  { time: "06:30", title: "Vitamin-B-Komplex", description: "", calories: 0 },
+  { time: "06:40", title: "Magnesium-Malat", description: "", calories: 0 },
+  { time: "06:45", title: "L-Glutamin", description: "5 g (mit Wasser)", calories: 0 },
+
+  { time: "07:00", title: "Medikamente + Supplemente (morgens)", description: "Eliquis/Apixaban 5 mg + Finerenon 10 mg + Vitamin D3/K2 + Omega-3 + Q10", calories: 0 },
+  { time: "07:30", title: "Kollagen-Drink", description: "10 g", calories: 36 },
+  { time: "08:00", title: "Frühstück (mehr Beeren)", description: "Haferflocken 50 g, Magerquark 150 g, Beeren 120 g, Leinöl 1 EL, Flohsamen 1 TL", calories: 360 },
+  { time: "09:00", title: "Herz / Niere", description: "Bisoprolol, Amlodipin, Ezetimib", calories: 0 },
+  { time: "09:30", title: "Eiweißblock (klein)", description: "Magerquark 150 g", calories: 120 },
+
+  { time: "12:30", title: "Mittagessen: Hamburger (Linsen-Bun)", description: "Linsen-Hamburgerbrötchen (60 g Linsen), Rinder-Patty 125 g (~15 % Fett), Salat (Blatt/Tomate/Gurke), Ketchup 20 g", calories: 507 },
+    
+  { time: "13:00", title: "Artischocke", description: "Leber / Galle", calories: 0 },
+
+  { time: "15:30", title: "Kleine Kohlenhydratportion", description: "Haferflocken 30 g + Wasser oder Tee", calories: 115 },
+  { time: "17:30", title: "L-Glutamin (2. Portion)", description: "3–5 g", calories: 0 },
+
+  { time: "18:00", title: "Abendmedikamente", description: "Eliquis/Apixaban 5 mg + Rosuvastatin + Ramipril", calories: 0 },
+  { time: "18:30", title: "Leicht & warm", description: "Buchweizen (gekocht) 60 g + Gemüse 250 g + Miso-Dashi 1–2 TL", calories: 220 },
+
+  { time: "20:30", title: "Zink", description: "nur mit Wasser", calories: 0 },
+  { time: "20:30", title: "Optional (wenn Hunger)", description: "Magerquark 100 g", calories: 70 },
+  { time: "22:00", title: "Magnesium-Bisglycinat", description: "", calories: 0 }
+];
